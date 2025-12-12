bd7a3eacac412a16dacf96275bd60678c226935e
Gotprgmer <gofiction95@gmail.com>
2025-12-12 17:34:04 +0900

    타입별 어노테이션 작성 및 테스트 추가, 파일객체를 통해 코드 조회 해봤음

---
+++ src/main/com/gotprgmer/pure_java/annotation/ClassBread.java
@@ -0,0 +1,24 @@
+package src.main.com.gotprgmer.pure_java.annotation;
+
+@MyInfoClass
+public class ClassBread extends Bread{
+    private String recipe;
+    private String cook;
+    public ClassBread(){
+    }
+//    public void setRecipe(String newRecipe){
+//        this.recipe = newRecipe;
+//    }
+//
+//    public void setCook(String newCook){
+//        this.cook = newCook;
+//    }
+//
+//    public String getCook() {
+//        return cook;
+//    }
+//
+//    public String getRecipe(){
+//        return this.recipe;
+//    }
+}
